293ebe7bfb325a1a2e1ba12985b07dfc8fe8c71d
Xuesong Wang <xwang445@pelican.ib.hpcc.ucr.edu>
2020-10-27 13:45:53 -0700

    include 0

--- sevenless.py
+++ sevenless.py
@@ -5,6 +5,8 @@
 #divisor=7
 #print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
 for i in range(100):
+    if i == 0:
+        print(i)
     if i % 7 == 0:
-      continue
+        continue
     print(i)
