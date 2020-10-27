72b88ce57cabe01db90d643d3d5b93687a470aaa
xuesong0724 <72754105+xuesong0724@users.noreply.github.com>
2020-10-27 12:30:50 -0700

    Update sevenless.py

--- sevenless.py
+++ sevenless.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-start = 0
-end   = 99
-divisor=7
-print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+#start = 0
+#end   = 99
+#divisor=7
+#print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+for i in range(100):
+    if i % 7 == 0:
+      continue
+    print(i)
