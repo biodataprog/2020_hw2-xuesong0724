8cdc14236667f69d7dfe1530e66e796b638d4e81
Xuesong Wang <xwang445@pelican.ib.hpcc.ucr.edu>
2020-10-28 22:05:22 -0700

    count_gene_number done

--- count_up.py
+++ count_up.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
-# this is a python script template
-# this next line will download the file using curl
+# download the gff file
+import urllib.request
+loadfile = "ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/gff3/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz"
+gff = urllib.request.urlopen(loadfile)
 
-gff="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz"
-fasta="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz"
-
-import os,gzip,itertools,csv,re
+# this is code which will count the number in gff
+import gzip
+with gzip.open(gff) as f:
+        i=0
+        for line in f:
+                linestrip = line.decode('UTF-8').strip().split('\t')
+                if linestrip[0][0] == "#":
+                        continue
+                elif linestrip[2] == "gene":
+                        i += 1
+        num=str(i)
+        print(("total gene number in the gff is ")+(num))
 
 # this is code which will parse FASTA files
 # define what a header looks like in FASTA format
